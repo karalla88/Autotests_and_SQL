faab647355209cf952d4594d3ae93821691a5983
karalla88 <karalla88@gmail.com>
2024-02-21 22:46:37 +0300

    Перенесли свежие файлы

---
+++ configuration.py
@@ -0,0 +1,6 @@
+# Карачинцева Ольга, 13-я когорта — Финальный проект. Инженер по тестированию плюс
+
+URL_SERVICE = "https://9db499ea-f40d-4e41-ba16-8ebbd4baea8f.serverhub.praktikum-services.ru"
+CREATE_COURIER = "/api/v1/courier"
+CREATE_ORDER = "/api/v1/orders"
+ORDER_INFORMATION = "/api/v1/orders/track?t="
---
+++ data.py
@@ -0,0 +1,13 @@
+order_body = {
+    "firstName": "Олег",
+    "lastName": "Ольгович",
+    "address": "Мовзолей, 1",
+    "metroStation": 258,
+    "phone": "+34916123451",
+    "rentTime": 5,
+    "deliveryDate": "2024-02-21",
+    "comment": "Вези котлеты",
+    "color": [
+        "BLACK"
+    ]
+}
